c3fb44d45b57c0735e63ee79574e14394b5bc7c6
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-12-30 12:50:34 -0600

    Initial Commit

---
+++ JabberClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JabberClient
+//
+//  Created by Eduardo Alvarado Díaz on 12/30/14.
+//  Copyright (c) 2014 Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JabberClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JabberClient
+//
+//  Created by Eduardo Alvarado Díaz on 12/30/14.
+//  Copyright (c) 2014 Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
